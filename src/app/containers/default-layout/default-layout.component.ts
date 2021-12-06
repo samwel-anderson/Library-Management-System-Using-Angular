7cb803969aa111d1c18efb15304fddda68e20b53
sammy <samwel.sirikwa@tra.go.tz>
2021-12-06 17:39:35 +0300

    Adding Dynamic Menu based on Permissions, Custom Html

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { navItems } from '../../_nav';
+import {TokenService} from '../../services/token.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,11 +9,20 @@ import { navItems } from '../../_nav';
 export class DefaultLayoutComponent implements OnInit {
   public sidebarMinimized = false;
   public navItems = navItems;
-
   isUserLoggedIn = false;
+
+  hasBooksMenu = false;
+
+  constructor(private tokenService: TokenService) {}
 
   ngOnInit() {
     const storeData = localStorage.getItem('isUserLoggedIn');
+    const permArray = this.tokenService.getPermissions();
+    const booksPermissions = ['view_books', 'add_books', 'change_books', 'delete_books'];
+    this.hasBooksMenu = permArray.some( ai => booksPermissions.includes(ai));
+
+
+
 
     if ( storeData != null && storeData === 'true') {
       this.isUserLoggedIn = true;
